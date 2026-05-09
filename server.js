a44d9b6fa3bfa9fc7faa04b95071176a963a0e1a
Karthik-12032005 <25250148karthik@staloysius.ac.in>
2026-05-09 17:43:23 +0530

    Fixed Render deployment

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
+// View engine
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+// Home route
 app.get("/", (req, res) => {
     res.render("index", { message: null });
 });
 
+// Form submit route
 app.post("/submit", (req, res) => {
-    const { username, email, phone, gender } = req.body;
+    const { username } = req.body;
 
     res.render("index", {
-       message: `Thank You ${username}! Your details were submitted successfully.`
+        message: `Thank You ${username}! Form Submitted Successfully`
     });
 });
 
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
